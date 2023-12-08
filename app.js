67c88d877f789577416a308f540acb82391a2959
Dmytro <pelexov.mitya@gmail.com>
2023-12-08 20:51:27 +0200

    modified design of slider

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let productsCountEl = document.getElementById("products-count");
-console.log(productsCountEl);
+// console.log(productsCountEl);
 
 let addToCartBtns = document.querySelectorAll(".btn-add");
-console.log(addToCartBtns);
+// console.log(addToCartBtns);
 
 // function clickButton() {
 //   console.log("clicked");
@@ -15,21 +15,29 @@ addToCartBtns.forEach((btn) =>
   })
 ); /*---> лічильник товарів в кошику */
 
-// -------------------------------------------------------------------------
+// ---------------реалізація лайк----------------------------------------------------------
 
 let like = document.querySelectorAll(".icon5");
-console.log(like);
+// console.log(like);
 
-function changeLike(item) {
-  let likeAtr = item.getAttribute("class");
-  if (likeAtr === "icon5") {
-    item.setAttribute("class", "icon5-active");
-  } else {
-    item.setAttribute("class", "icon5");
-  }
-}
+// ----------------------варіант рішення № 1 --------------
 
-like.forEach((item) => item.addEventListener("click", changeLike(item)));
+// function changeLike(item) {
+//   let likeAtr = item.getAttribute("class");
+//   if (likeAtr === "icon5") {
+//     item.setAttribute("class", "icon5-active");
+//   } else {
+//     item.setAttribute("class", "icon5");
+//   }
+// }
+
+// like.forEach((item) =>
+//   item.addEventListener("click", () => {
+//     changeLike(item);
+//   })
+// );
+
+// ------------------ варіант рішення № 2 ----------------
 
 // like.forEach((item) =>
 //   item.addEventListener("click", function () {
@@ -37,17 +45,19 @@ like.forEach((item) => item.addEventListener("click", changeLike(item)));
 //   })
 // );
 
-// like.forEach((item) =>
-//   item.addEventListener("click", function () {
-//     let iconAtr = item.getAttribute("class");
-//     if (iconAtr === "icon5") {
-//       item.setAttribute("class", "icon5-active");
-//     } else {
-//       item.setAttribute("class", "icon5");
-//     }
-//   })
-// );
+// -----------------варіант рішення № 3 ----------------
 
+like.forEach((item) =>
+  item.addEventListener("click", function () {
+    let iconAtr = item.getAttribute("class");
+    if (iconAtr === "icon5") {
+      item.setAttribute("class", "icon5-active");
+    } else {
+      item.setAttribute("class", "icon5");
+    }
+  })
+);
+// ---------------------- експеримент --------
 // like.forEach((item) =>
 //   item.addEventListener("click", function () {
 //     let iconAtr = item.getAttribute("class");
@@ -60,3 +70,59 @@ like.forEach((item) => item.addEventListener("click", changeLike(item)));
 // );
 
 // ----------------------------
+
+// ----------------вспливаюча форма---------------
+let moreDetailsBtns = document.querySelectorAll(".btn-more");
+console.log(moreDetailsBtns);
+let modal = document.querySelector(".modal");
+console.log(modal);
+let closeBtn = document.querySelector(".btn-close");
+console.log(closeBtn);
+
+moreDetailsBtns.forEach((item) => {
+  item.addEventListener("click", function () {
+    // modal.style.display = "block";
+    // просто вручну змінюємо властивість класу .modal
+    // modal.style.backgroundColor = "red";
+    // Правильне рішення
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+  });
+});
+
+closeBtn.addEventListener("click", function () {
+  modal.classList.add("hide");
+  modal.classList.remove("show");
+});
+// Або коротше
+// closeBtn.addEventListener("click", closeModal);
+// функція closeModal прописана нижче
+
+// зробимо закривання форми при кліку поза формою
+function closeModal() {
+  modal.classList.add("hide");
+  modal.classList.remove("show");
+}
+
+modal.addEventListener("click", function (e) {
+  if (e.target === modal) {
+    closeModal();
+  }
+  // ПРИНЦИП ДІЇ: функція closeModal додає клас для modal, ховаючи його.
+  // Event (у нас e) - об'єкт події.
+  //  e.target - показує той елемент, по якому ми клікаємо
+  // ми пишемо: якщо e.target===modal(а в нас вони дорівнюють, оскільки затемнення- це modal) то сховати modal )
+
+  if (e.target === document.querySelector(".form-more-details h3")) {
+    let modalH_3 = document.querySelector(".form-more-details h3");
+    modalH_3.style.backgroundColor = "green";
+    // це для зразка
+  }
+});
+
+//-------------------слайдер----------------------------------
+
+$(".slider-block").slick({
+  dots: true,
+  infinite: true,
+});
